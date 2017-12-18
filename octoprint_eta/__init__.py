78a9ff58152b14c9ecfc6b6dc0acf874c69a6c79
Pablo Ventura <pablo@claude.workgroup>
2017-12-18 14:10:30 -0300

    ahora si

--- octoprint_eta/__init__.py
+++ octoprint_eta/__init__.py
@@ -11,47 +11,21 @@ class DisplayETAPlugin(octoprint.plugin.ProgressPlugin,
                        octoprint.plugin.EventHandlerPlugin):
 
     def __init__(self):
-        self.eta_string = "-"
-        self.eta_strftime = "%H:%M:%S"
-        self.timer = RepeatedTimer(15.0, DisplayETAPlugin.fromTimer, args=[self], run_first=True,)
-
-    def fromTimer(self):
-        self.eta_string = self.calculate_ETA()
-        self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
-        
-        
-    def calculate_ETA(self):
-        currentData = self._printer.get_current_data()
-        if not currentData["progress"]["printTimeLeft"]:
-            return "-"
-        current_time = time.localtime(time.time())
-        finish_time = time.localtime(time.time() + currentData["progress"]["printTimeLeft"])
-        strtime = time.strftime(self.eta_strftime, finish_time)
-        strdate = ""
-        if finish_time.tm_mday > current_time.tm_mday:
-            if finish_time.tm_mday == current_time.tm_mday + 1:
-                strdate = " Tomorrow"
-            else:
-                strdate = " Day %s" % finish_time.tm_mday
-        return strtime + strdate
-            
-        
-        
-    def on_print_progress(self,storage, path, progress):
-        self.eta_string = self.calculate_ETA()
-        self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
-        
+        self.activated = True
+        self.timer = RepeatedTimer(5.0, DisplayETAPlugin.fromTimer, args=[self], run_first=False,)
+        self.timer.start()
+        self.currentFile=None
     def on_event(self,event, payload):
-        if event.startswith('Print'):
-            if event not in {"PrintStarted","PrintResumed"}:
-                self.eta_string="-"
-                self.timer.cancel()
-            else:
-                self.eta_string = self.calculate_ETA()
-                self.timer.cancel()
-                self.timer = RepeatedTimer(10.0, DisplayETAPlugin.fromTimer, args=[self], run_first=True,)
-                self.timer.start()
-            self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
+        if event == "FileSelected":
+            self.currentFile=payload["file"]
+            
+    def fromTimer(self):
+        self._logger.info("The Timer Has Fired")
+        self._logger.info(self._printer.get_current_job())
+        self._printer.select_file('/home/pablo/.octoprint/uploads/20mm_hollow_cube.gcode', False, printAfterSelect=True)
+
+        self.timer.cancel()
+        #self._plugin_manager.send_plugin_message(self._identifier, dict(eta_string=self.eta_string))
             
     def get_assets(self):
         return {
